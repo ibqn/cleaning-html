327bb1e89b39b59ddc2ea25cfc5beb2d20082792
ibqn <evgen.ibqn@gmail.com>
2022-07-18 00:38:30 +0200

    adding style switcher

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ const headerBackground = () => {
 }
 
 headerBackground()
+
+const styleSwitcherToggle = () => {
+  const styleSwitcher = document.querySelector('.js-style-switcher')
+  const styleSwitcherToggler = document.querySelector(
+    '.js-style-switcher-toggler'
+  )
+
+  styleSwitcherToggler.addEventListener('click', () => {
+    styleSwitcher.classList.toggle('open')
+    styleSwitcherToggler
+      .querySelector('i')
+      .classList.toggle('fa-times')
+      .toggle('fa-cog')
+  })
+}
+
+styleSwitcherToggle()
